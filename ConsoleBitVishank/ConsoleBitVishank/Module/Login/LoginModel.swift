6764e9570cd4a2f33002c624efa47813a25ea3d3
Vishank Raghav <vishank13@gmail.com>
2022-11-02 17:40:06 +0530

    Login Structure

---
+++ ConsoleBitVishank/ConsoleBitVishank/Module/Login/LoginModel.swift
@@ -0,0 +1,26 @@
+//
+//  LoginModel.swift
+//  ConsoleBitVishank
+//
+//  Created by Vishank Raghav on 02/11/22.
+//
+
+import Foundation
+
+struct Login: Codable {
+    let access, refresh: String?
+    let user: User?
+}
+
+// MARK: - User
+struct User: Codable {
+    let id: Int?
+    let firstName, lastName, email: String?
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case email
+    }
+}
---
+++ ConsoleBitVishank/ConsoleBitVishank/Utilities/AppConstants.swift
@@ -0,0 +1,8 @@
+//
+//  AppConstants.swift
+//  ConsoleBitVishank
+//
+//  Created by Vishank Raghav on 02/11/22.
+//
+
+import Foundation
